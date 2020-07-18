82e89d6eed6dce735cb086c29ef2f019afc350d0
Gustavo <gustavo.oliveira@tapps.com.br>
2020-07-18 13:44:18 -0300

    Added A Star

---
+++ A Star/AStar.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+
+namespace LegendaryTools.AI.AStar
+{
+    public interface IAStar<T>
+    {
+        T[] Neighbors(T node);
+
+        float Heuristic(T nodeA, T nodeB);
+    }
+
+    public class AStar<T>
+    {
+        private static readonly Dictionary<T, AStarNode> cachedNodes = new Dictionary<T, AStarNode>();
+        private readonly IAStar<T> map;
+
+        public AStar(IAStar<T> map)
+        {
+            this.map = map;
+        }
+
+        public T[] FindPath(T startLocation, T endLocation)
+        {
+            Dictionary<AStarNode, AStarNode> cameFrom = new Dictionary<AStarNode, AStarNode>();
+            Dictionary<AStarNode, float> costSoFar = new Dictionary<AStarNode, float>();
+            PriorityQueue<AStarNode> open = new PriorityQueue<AStarNode>();
+
+            cleanNodesData();
+
+            AStarNode startNode = getFromCache(startLocation);
+            AStarNode endNode = getFromCache(endLocation);
+
+            open.Enqueue(startNode);
+            cameFrom[startNode] = startNode;
+            costSoFar[startNode] = 0;
+
+            AStarNode currentNode;
+            while (open.Count > 0)
+            {
+                currentNode = open.Dequeue();
+
+                if (currentNode.Location.Equals(endLocation))
+                {
+                    List<T> path = new List<T>();
+                    currentNode = endNode;
+
+                    while (currentNode != startNode)
+                    {
+                        path.Add(currentNode.Location);
+                        currentNode = cameFrom[currentNode];
+                    }
+
+                    path.Reverse();
+                    return path.ToArray();
+                }
+
+                T[] neighbours = map.Neighbors(currentNode.Location);
+                AStarNode currentNeighborsNode = null;
+                for (int i = 0; i < neighbours.Length; i++)
+                {
+                    currentNeighborsNode = getFromCache(neighbours[i]);
+                    float newCost = costSoFar[currentNode];
+                    if (!costSoFar.ContainsKey(currentNeighborsNode) || newCost < costSoFar[currentNeighborsNode])
+                    {
+                        costSoFar[currentNeighborsNode] = newCost;
+                        currentNeighborsNode.Score =
+                            newCost + map.Heuristic(currentNeighborsNode.Location, endLocation);
+                        open.Enqueue(currentNeighborsNode);
+                        cameFrom[currentNeighborsNode] = currentNode;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static void cleanNodesData()
+        {
+            foreach (KeyValuePair<T, AStarNode> pair in cachedNodes)
+            {
+                pair.Value.Clean();
+            }
+        }
+
+        private static AStarNode getFromCache(T node)
+        {
+            if (!cachedNodes.ContainsKey(node))
+            {
+                cachedNodes.Add(node, new AStarNode(node));
+            }
+
+            return cachedNodes[node];
+        }
+
+        private class AStarNode : IPriorityQueueNode
+        {
+            public readonly T Location;
+
+            public float Score;
+
+            public AStarNode(T location)
+            {
+                Location = location;
+            }
+
+            public float Priority
+            {
+                get => Score;
+                set => Score = value;
+            }
+
+            public override int GetHashCode()
+            {
+                return Location.GetHashCode();
+            }
+
+            public void Clean()
+            {
+                Score = 0;
+            }
+        }
+    }
+}
---
+++ A Star/PriorityQueue.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace LegendaryTools.AI.AStar
+{
+    public interface IPriorityQueueNode
+    {
+        float Priority { get; set; }
+    }
+
+    public class PriorityQueue<T>
+        where T : IPriorityQueueNode
+    {
+        private readonly List<T> queue = new List<T>();
+
+        public int Count => queue.Count;
+
+        public void Enqueue(T item)
+        {
+            queue.Add(item);
+            sort(0, queue.Count - 1);
+        }
+
+        public T Dequeue()
+        {
+            T item = queue[0];
+            queue.RemoveAt(0);
+            return item;
+        }
+
+        //quick sort algorithm
+        private void sort(int startIndex, int endIndex)
+        {
+            if (startIndex < endIndex)
+            {
+                float p = queue[startIndex].Priority;
+                int i = startIndex + 1;
+                int f = endIndex;
+
+                while (i <= f)
+                {
+                    if (queue[i].Priority <= p)
+                    {
+                        i++;
+                    }
+                    else if (p < queue[f].Priority)
+                    {
+                        f--;
+                    }
+                    else
+                    {
+                        float swap = queue[i].Priority;
+                        queue[i].Priority = queue[f].Priority;
+                        queue[f].Priority = swap;
+                        i++;
+                        f--;
+                    }
+                }
+
+                queue[startIndex].Priority = queue[f].Priority;
+                queue[f].Priority = p;
+
+                sort(startIndex, f - 1);
+                sort(f + 1, endIndex);
+            }
+        }
+    }
+}
